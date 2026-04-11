d26df76d00d81de243b20f365ee35fcd05e8b868
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2026-04-11 16:55:24 +0200

    add endpoint for checking if bot is running

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import dotenv from 'dotenv';
 import { startHeartbeat } from './src/config/db.js';
 import { startBot } from './src/services/whatsappService.js';
+import express from 'express'
 
 dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/ping', (req, res) => {
+    res.send('Admin WhatsApp Bot is running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 async function main() {
     try {
